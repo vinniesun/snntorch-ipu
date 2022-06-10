fcfd3e78d88d7e2580e827aa8a2a1ccd0a09b850
Jason Eshraghian <jeshraghian@gmail.com>
2022-06-10 18:15:02 +0800

    Bump version: 0.5.4 → 0.5.5

--- _version.py
+++ _version.py
@@ -1,2 +1,2 @@
 # fmt: off
-__version__ = '0.5.4'
+__version__ = '0.5.5'
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst", encoding="utf-8") as readme_file:
 #     history = history_file.read()
 
 # fmt: off
-__version__ = '0.5.4'
+__version__ = '0.5.5'
 # fmt: on
 
 requirements = [
