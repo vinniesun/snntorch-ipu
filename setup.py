9c1f62ff8b2d1509067a1322c1cdde0cbd8873c9
Jason Eshraghian <jeshraghian@gmail.com>
2022-06-13 14:56:14 +0800

    Bump version: 0.5.9 → 0.5.10

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst", encoding="utf-8") as readme_file:
 #     history = history_file.read()
 
 # fmt: off
-__version__ = '0.5.9'
+__version__ = '0.5.10'
 # fmt: on
 
 requirements = [
