edad0d38ec733d78bda0fc72806e4c8fdaa00ca9
Jason Eshraghian <jasonh@lr24-4a-poplar-1.openstack-dev.graphcore.ai>
2022-06-15 03:37:56 +0000

    modified __init__.py to compile so files if they aren't

--- snntorch/__init__.py
+++ snntorch/__init__.py
@@ -1,2 +1,15 @@
 from ._version import __version__
 from ._neurons import *
+
+import os
+import subprocess
+
+CWD = os.path.dirname(__file__)
+if not os.path.isfile(os.path.join(CWD, "so_file/heaviside_custom_ops.so")) or \
+        not os.path.isfile(os.path.join(CWD, "so_file/straight_through_estimator_custom_ops.so")) or \
+        not os.path.isfile(os.path.join(CWD, "fast_sigmoid_custom_ops.so")):
+            print("Missing so files, will compile them now!")
+            
+            custom_ops_path = os.path.join(CWD, "custom_ops")
+            subprocess.call(["make", "-C", custom_ops_path])
+            print("Successfully created!")
