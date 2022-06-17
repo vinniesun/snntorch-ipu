dbeaf9cb9dfacc5aca8eecd74b8aa4507dad5b9b
Jason Eshraghian <jeshraghian@gmail.com>
2022-06-17 14:52:41 +0800

    Bump version: 0.5.18 → 0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath(".."))
 import snntorch
 
 # fmt: off
-__version__ = '0.5.18'
+__version__ = '0.6.0'
 # fmt: on
 
 
--- snntorch/_version.py
+++ snntorch/_version.py
@@ -1,2 +1,2 @@
 # fmt: off
-__version__ = '0.5.18'
+__version__ = '0.6.0'
